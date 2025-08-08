55c1c6fa24b57af17c3353b4cf1b5ccceaeb74cc
Abdullah <abdullahluqmandudebro@gmail.com>
2025-08-08 18:09:30 +0500

    Fix for Vercel deployment

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
   });
 
-app.listen(3001, () => {
-    console.log('Server is running on http://localhost:3001');
-  });
+// app.listen(3001, () => {
+//     console.log('Server is running on http://localhost:3001');
+//   });
+module.exports = app;
